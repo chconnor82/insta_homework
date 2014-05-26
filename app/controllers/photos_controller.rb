de8aa8d49de9391ff60e679ee681b511a736624d
Charlie Connor <chconnor@gmail.com>
2014-05-26 02:23:04 -0500

    Still Missing right values
    Comments show up as code
    
    Create Comment does not pass along user ID

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,6 +5,7 @@ class PhotosController < ApplicationController
 
   def show
     @photo = Photo.find(params[:id])
+    # @photo.responses = params[:responses]
   end
 
   def new
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,5 +3,8 @@ class Photo < ActiveRecord::Base
   belongs_to :user
   has_many :favorites
   has_many :fans, :through => :favorites, :source => :user
+  has_many :comments
+  has_many :responses, :through => :comments, :source => :photo
+
 
 end
