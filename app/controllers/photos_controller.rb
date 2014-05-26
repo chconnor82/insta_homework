a64c05b631ce4c8e2bee69cdfe365bfc51b5ccd8
Charlie Connor <chconnor@gmail.com>
2014-05-26 11:20:00 -0500

    Made the application smarter
    Added smarter actions about signed in users vs. signed out

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -16,7 +16,7 @@ class PhotosController < ApplicationController
     @photo = Photo.new
     @photo.image_url = params[:image_url]
     @photo.caption = params[:caption]
-    @photo.user_id = params[:user_id]
+    @photo.user_id = current_user.id
 
     if @photo.save
       redirect_to "/photos", :notice => "Photo created successfully."
@@ -34,7 +34,7 @@ class PhotosController < ApplicationController
 
     @photo.image_url = params[:image_url]
     @photo.caption = params[:caption]
-    @photo.user_id = params[:user_id]
+    @photo.user_id = current_user.id
 
     if @photo.save
       redirect_to "/photos", :notice => "Photo updated successfully."
